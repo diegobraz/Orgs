ead8cb6ddac99375dd6456abfdc0c534600dc54c
Diego_braz <diegobr73@hotmail.com>
2022-03-31 08:58:44 -0300

    refactor authentication and salve user instace

--- app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
@@ -2,10 +2,22 @@ package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsuarioDao {
     @Insert
     suspend fun save(usuario: Usuario)
+
+    @Query(
+        """SELECT * FROM usuario
+        WHERE id = :usuarioId 
+        AND senha = :senha"""
+    )
+    suspend fun autentica(usuarioId: String, senha: String): Usuario?
+
+    @Query("SELECT * FROM usuario WHERE id = :usuarioId")
+    fun buscaPorId(usuarioId: String): Flow<Usuario>
 }
